29e2a9c7e4cda8573bdcd64940c6453ca9ab802a
rioto.nagano <kingdom950927@gmail.com>
2026-07-01 07:10:54 +0900

    add: CloudFrontディストリビューションやOACなどの作成

--- terraform/modules/s3_cloudfront/main.tf
+++ terraform/modules/s3_cloudfront/main.tf
@@ -1,3 +1,4 @@
+# オリジン
 resource "aws_s3_bucket" "main" {
   bucket = "frontend-static-content-bucket-${var.env}-rio-20260630"
 
@@ -6,3 +7,170 @@ resource "aws_s3_bucket" "main" {
     Environment = var.env
   }
 }
+
+# バケットポリシー
+data "aws_iam_policy_document" "origin_bucket_policy" {
+  statement {
+    sid    = "AllowCloudFrontServicePrincipalReadWrite"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject" # 読み取りのみ許可
+    ]
+
+    resources = [
+      "${aws_s3_bucket.main.arn}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.s3_distribution.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "main" {
+  bucket = aws_s3_bucket.main.bucket
+  policy = data.aws_iam_policy_document.origin_bucket_policy.json
+}
+
+locals {
+  s3_origin_id = "myS3Origin"
+  my_domain    = "mydomain.com"
+}
+
+# data "aws_acm_certificate" "my_domain" {
+#   region   = "us-east-1"
+#   domain   = "*.${local.my_domain}"
+#   statuses = ["ISSUED"]
+# }
+
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "default-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.main.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  # aliases = ["mysite.${local.my_domain}", "yoursite.${local.my_domain}"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["JP"]
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    # acm_certificate_arn = data.aws_acm_certificate.my_domain.arn
+    # ssl_support_method  = "sni-only"
+    cloudfront_default_certificate = true
+  }
+}
+
+# Create Route53 records for the CloudFront distribution aliases
+# data "aws_route53_zone" "my_domain" {
+#   name = local.my_domain
+# }
+
+# resource "aws_route53_record" "cloudfront" {
+#   for_each = aws_cloudfront_distribution.s3_distribution.aliases
+#   zone_id  = data.aws_route53_zone.my_domain.zone_id
+#   name     = each.value
+#   type     = "A"
+
+#   alias {
+#     name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+#     zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+#     evaluate_target_health = false
+#   }
+# }
