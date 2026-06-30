415b73ae1ce6ae698983e2f6fcd06d5e0df69aca
rioto.nagano <kingdom950927@gmail.com>
2026-07-01 05:30:10 +0900

    add: 変数に変更

--- terraform/modules/s3_cloudfront/main.tf
+++ terraform/modules/s3_cloudfront/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "main" {
-  bucket = "frontend-static-content-bucket-dev-rio-20260630"
+  bucket = "frontend-static-content-bucket-${var.env}-rio-20260630"
 
   tags = {
     Name        = "Frontend bucket"
